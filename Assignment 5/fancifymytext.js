95724002c7199fa5435460875d4647c45e35aac1
Jonathan <113552484+j-medrano@users.noreply.github.com>
2024-05-08 18:00:01 -0700

    Add files via upload
    Assignment 5

---
+++ Assignment 5/fancifymytext.js
@@ -0,0 +1,53 @@
+
+
+function alertBox(){
+  alert("hello,world")
+
+}
+
+function biggerText(){
+  document.getElementById("textBox").style.fontSize = "24pt"
+}
+
+
+function fancyChange(){
+  alert("FancyShmancy has been clicked!")
+  document.getElementById("textBox").style.fontWeight= "bold"
+  document.getElementById("textBox").style.color= "blue"
+  document.getElementById("textBox").style.textDecoration= "underline"
+
+}
+
+function boringChange(){
+  alert("BoringBetty has been clicked!")
+  document.getElementById("textBox").style.fontWeight= "normal"
+  document.getElementById("textBox").style.color= "black"
+  document.getElementById("textBox").style.textDecoration= "none"
+
+}
+
+function moo(){
+  var inputTextBox = document.getElementById("textBox");
+  text= inputTextBox.value.toUpperCase();
+
+  parts = text;
+
+  if(parts.includes("!")){
+    parts = parts.split("!")
+    parts = parts.join("-Moo! ")
+
+  }
+
+  if(parts.includes(".")){
+    parts = parts.split(".")
+    parts = parts.join("-Moo. ")
+
+  }
+  alert(parts.includes("?"))
+  if(parts.includes("?")){
+    parts = parts.split("?")
+    parts = parts.join("-Moo? ")
+
+  }
+  inputTextBox.value = parts
+}
